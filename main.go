1973e35587516f369ab7bbd14388d60574279170
Arinomi <odin.bernhofsen@gmail.com>
2018-10-07 17:59:54 +0200

    Added basic uptime at /api/

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+var startTime = time.Now()
+
+// TODO: Make this print out in ISO 8061 standard
+func uptime() time.Duration {
+	return time.Since(startTime)
+}
+
+/*type apiInfo struct {
+	Uptime		time.Duration 	`json:"uptime"`
+	Info		string 			`json:"info"`
+	Version		string 			`json:"version"`
+}*/
+
+func handlerAPI(w http.ResponseWriter, r *http.Request) {
+	http.Header.Add(w.Header(), "content-type", "application/json")
+	fmt.Fprintln(w, "Uptime:", uptime())
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+	http.HandleFunc("/api/", handlerAPI)
+	http.ListenAndServe(":8080", nil)
 }
