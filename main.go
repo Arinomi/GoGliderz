aa47a8b3dcfd93ffb1d9f63fb918ada9fa09cbf7
Arinomi <oabernho@stud.ntnu.no>
2018-10-11 20:03:22 +0200

    Implemented /igcinfo/api/igc/{id}, also rewrote some code to be able to use httprouter. This package makes accepting info from the URL much easier, resulting in less code.

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/julienschmidt/httprouter"
 	"github.com/marni/goigc"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -49,7 +51,7 @@ func newTrack(url string) {
 	ids = append(ids, len(ids)+1)
 }
 
-func handlerAPI(w http.ResponseWriter, r *http.Request) {
+func handlerAPI(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	http.Header.Add(w.Header(), "content-type", "application/json")
 
 	info := apiInfo{uptime(), "Service for IGC tracks.", "v1"}
@@ -62,7 +64,7 @@ func handlerAPI(w http.ResponseWriter, r *http.Request) {
 	w.Write(jsresp)
 }
 
-func handlerIGC(w http.ResponseWriter, r *http.Request) {
+func handlerIGC(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	http.Header.Add(w.Header(), "content-type", "application/json")
 
 	switch r.Method {
@@ -76,8 +78,8 @@ func handlerIGC(w http.ResponseWriter, r *http.Request) {
 			w.Write(jsresp)
 		} else {
 			http.Error(w, "No files found", http.StatusNotFound)
+			return
 		}
-
 	case "POST":
 		http.Error(w, "Not implemented", http.StatusNotImplemented)
 		return
@@ -87,12 +89,44 @@ func handlerIGC(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func handlerID(w http.ResponseWriter, r *http.Request, param httprouter.Params) {
+	if len(trackMAP) > 0 && len(ids) > 0 {
+		http.Header.Add(w.Header(), "content-type", "application/json")
+		id, err := strconv.Atoi(param[0].Value)
+		if err != nil {
+			http.Error(w, "Please provide a valid ID.", http.StatusBadRequest)
+			return
+		} else {
+			_, ok := trackMAP[id]
+			if ok {
+				fmt.Println(id)
+				jsresp, err := json.Marshal(trackMAP[id])
+				if err != nil {
+					http.Error(w, err.Error(), http.StatusInternalServerError)
+					return
+				}
+				w.Write(jsresp)
+			} else {
+				http.Error(w, "Given ID not found", http.StatusNotFound)
+			}
+		}
+	} else {
+		http.Error(w, "No files found", http.StatusNotFound)
+	}
+}
+
 func main() {
 	fmt.Println("Running...")
+	router := httprouter.New()
 	newTrack("http://skypolaris.org/wp-content/uploads/IGS%20Files/Madrid%20to%20Jerez.igc")
 	newTrack("http://skypolaris.org/wp-content/uploads/IGS%20Files/Jarez%20to%20Senegal.igc")
 	newTrack("http://skypolaris.org/wp-content/uploads/IGS%20Files/Boavista%20Medellin.igc")
-	http.HandleFunc("/igcinfo/api", handlerAPI)
+	/*http.HandleFunc("/igcinfo/api", handlerAPI)
 	http.HandleFunc("/igcinfo/api/igc", handlerIGC)
-	http.ListenAndServe("127.0.0.1:8080", nil)
+	http.HandleFunc("/igcinfo/api/igc/:id", handlerID)*/
+	router.GET("/igcinfo/api", handlerAPI)
+	router.GET("/igcinfo/api/igc", handlerIGC)
+	router.GET("/igcinfo/api/igc/:id", handlerID)
+
+	http.ListenAndServe("127.0.0.1:8080", router)
 }
